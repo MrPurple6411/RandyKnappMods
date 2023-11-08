ecadf07b5107c4180a9899f14ed30d8896f8d1a9
MrPurple6411 <mrpurple6411@gmail.com>
2023-11-08 15:08:00 -0700

    smlhelper incomp

--- SubnauticaModSystem/BetterScannerBlips/Plugin.cs
+++ SubnauticaModSystem/BetterScannerBlips/Plugin.cs
@@ -6,7 +6,8 @@ using BepInEx;
 using BepInEx.Logging;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-[BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, Nautilus.PluginInfo.PLUGIN_VERSION)]
+[BepInIncompatibility("com.ahk1221.smlhelper")]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger { get; private set; }
--- SubnauticaModSystem/QuitToDesktop/Plugin.cs
+++ SubnauticaModSystem/QuitToDesktop/Plugin.cs
@@ -6,7 +6,8 @@ using BepInEx;
 using QuitToDesktop.Configuration;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-[BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, Nautilus.PluginInfo.PLUGIN_VERSION)]
+[BepInIncompatibility("com.ahk1221.smlhelper")]
 public class Plugin : BaseUnityPlugin
 {
     public void Awake()
