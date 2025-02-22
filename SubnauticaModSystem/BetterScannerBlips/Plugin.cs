d14498d39e6ac2a108b3e835c547abac94fa0dbe
Joshua Gibbs <mrpurple6411@gmail.com>
2025-02-22 13:01:58 -0700

    process checks

--- SubnauticaModSystem/BetterScannerBlips/Plugin.cs
+++ SubnauticaModSystem/BetterScannerBlips/Plugin.cs
@@ -8,6 +8,11 @@ using BepInEx.Logging;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, Nautilus.PluginInfo.PLUGIN_VERSION)]
 [BepInIncompatibility("com.ahk1221.smlhelper")]
+#if SUBNAUTICA
+[BepInProcess("Subnautica.exe")]
+#elif BELOWZERO
+[BepInProcess("SubnauticaZero.exe")]
+#endif
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger { get; private set; }
--- SubnauticaModSystem/QuitToDesktop/Plugin.cs
+++ SubnauticaModSystem/QuitToDesktop/Plugin.cs
@@ -8,6 +8,11 @@ using QuitToDesktop.Configuration;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, Nautilus.PluginInfo.PLUGIN_VERSION)]
 [BepInIncompatibility("com.ahk1221.smlhelper")]
+#if SUBNAUTICA
+[BepInProcess("Subnautica.exe")]
+#elif BELOWZERO
+[BepInProcess("SubnauticaZero.exe")]
+#endif
 public class Plugin : BaseUnityPlugin
 {
     public void Awake()
